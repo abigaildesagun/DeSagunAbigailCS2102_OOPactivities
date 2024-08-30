15ad9a699ca5f787480b412c559b5b076d88c3db
Abigail De Sagun <24-02836@g.batstate-u.edu.ph>
2024-08-30 14:21:35 +0800

    Create Welcome.java

---
+++ Exercise Sets/Exercise Set 01/Welcome.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Welcome {
+    public static void main (String[]args){
+        Scanner input=new Scanner (System.in);
+
+        System.out.println("Welcome to CS 211: Object-oriented Programming!");
+        System.out.print("Enter your name: ");
+        String name=input.nextLine();
+        System.out.println("This course will be fun, " + name + "!");
+    }
+}
